82878f8797b2d194d9130ee15a25128796b42162
GithminiSandupama <164888580+GithminiSandupama@users.noreply.github.com>
2025-03-07 11:09:12 +0530

    feat: import Category module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { AppService } from './app.service';
 import { BestSelling, FeaturedItems, TodaysDeals } from './app.entity';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { Category } from './category/category.entity';
+import { CategoryModule } from './category/category.module';
 
 @Module({
   imports: [
@@ -15,15 +17,21 @@ import { join } from 'path';
       username: 'root',
       password: 'Password12',
       database: 'homepage',
-      entities: [BestSelling, FeaturedItems, TodaysDeals],
+      entities: [BestSelling, FeaturedItems, TodaysDeals, Category],
       synchronize: false,
     }),
-    TypeOrmModule.forFeature([BestSelling, FeaturedItems, TodaysDeals]),
+    TypeOrmModule.forFeature([
+      BestSelling,
+      FeaturedItems,
+      TodaysDeals,
+      Category,
+    ]),
 
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'assets'),
       serveRoot: '/images',
     }),
+    CategoryModule,
   ],
   controllers: [AppController],
   providers: [AppService],
